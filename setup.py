7d61cc0518b6f91e1a8363e82cfd5f3793239515
belledon <belledon@mit.edu>
2019-06-06 12:11:15 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="experiment",
+    name="galileo_ramp",
     version="0.0.1",
     author="Mario Belledonne",
     author_email="mbelledonne@gmail.com",
     description="Human Galileo experiment module",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages = ['experiment'],
+    packages = ['galileo_ramp'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
