1054f3976d141cd702fd5830d5dccc2e254a388d
belledon <belledon@mit.edu>
2019-11-01 10:18:03 -0400

    generalized

--- inference/dist.jl
+++ inference/dist.jl
@@ -23,24 +23,21 @@ function Gen.random(::RandomVec, mu::Vector{U}, noise::T) where {U<:Real,T<:Real
 end;
 (::RandomVec)(mu, noise) = random(RandomVec(), mu, noise)
 
-struct NoisyMatrix <: Gen.Distribution{Matrix{Float64}} end
+struct NoisyMatrix <: Gen.Distribution{Array{Float64}} end
 
 const mat_noise = NoisyMatrix()
 
-function Gen.logpdf(::NoisyMatrix, x::Matrix{Float64}, mu::Matrix{U}, noise::T) where {U<:Real,T<:Real}
+function Gen.logpdf(::NoisyMatrix, x::Array{Float64}, mu::Array{U}, noise::T) where {U<:Real,T<:Real}
     var = noise * noise
     diff = x - mu
     vec = diff[:]
     return -(vec' * vec)/ (2.0 * var) - 0.5 * log(2.0 * pi * var)
 end;
 
-function Gen.random(::NoisyMatrix, mu::Matrix{U}, noise::T) where {U<:Real,T<:Real}
+function Gen.random(::NoisyMatrix, mu::Array{U}, noise::T) where {U<:Real,T<:Real}
     mat = copy(mu)
-    (w, h) = size(mu)
-    for i=1:w
-        for j=1:h
-            mat[i, j] = mu[i, j] + randn() * noise
-        end
+    for i in CartesianIndeces(mu)
+        mat[i] = mu[i] + randn() * noise
     end
     return mat
 end;
