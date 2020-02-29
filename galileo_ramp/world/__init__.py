8fc19ef10efc16d621ec5357fbc8e7546aec81cf
belledon <belledon@mit.edu>
2020-02-29 16:40:17 -0500

    removed python src (now a dep)

--- galileo_ramp/world/__init__.py
+++
@@ -1,4 +0,0 @@
-""" Describes the Generative models (geometry, physics, rendering)
-
-.. module author:: Mario Belledonne <belledon@mit.edu>
-"""
--- galileo_ramp/world/render/__init__.py
+++
@@ -1,9 +0,0 @@
-""" Defines rendering aspects of Generative Model
-.. module:: world
-   :platform: Unix, Windows
-   :synopsis: Describes aspects of the Generative model related to rendering
-
-.. moduleauthor:: Mario Belledonne <belledon@mit.edu>
-
-
-"""
