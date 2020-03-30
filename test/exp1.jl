42cc79e0478e63a512a20a84ed362d03bf3e603f
belledon <mbelledonne@gmail.com>
2020-03-30 19:42:44 -0400

    closes #9

--- test/exp1.jl
+++ test/exp1.jl
@@ -1,4 +1,4 @@
-run_exp1_trial("/databases/exp1.hdf5", 1, 1, 0.1, nothing);
+seq_inference("/databases/exp1.hdf5", 1, 1, 0.1; out = nothing);
 println("finished first run");
-@time run_exp1_trial("/databases/exp1.hdf5", 1, 1, 0.1, "test.jld2");
-@time run_exp1_trial("/databases/exp1.hdf5", 1, 10, 0.1, "test.jld2");
+@time seq_inference("/databases/exp1.hdf5", 1, 1, 0.1; out = "test.jld2");
+@time seq_inference("/databases/exp1.hdf5", 1, 10, 0.1; out = "test.jld2");
