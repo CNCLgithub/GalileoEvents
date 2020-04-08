b85f13c36595a5378cbf2a9a68cac9157bac88a8
belledon <mbelledonne@gmail.com>
2020-04-08 11:13:10 -0400

    working on visualization

--- scripts/inference/visualize_exp1_pf.jl
+++ scripts/inference/visualize_exp1_pf.jl
@@ -7,6 +7,36 @@ using Gen_Compose
 using GalileoRamp
 using FileIO
 
+using ArgParse
+using Base.Filesystem
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "--dataset"
+        help = "Exp1 Dataset"
+        arg_type = String
+        default = "/databases/exp1.hdf5"
+
+        "--particles"
+        help = "Number of particles"
+        arg_type = Int
+        default = 1
+
+        "--obs_noise"
+        help = "Observation noise"
+        arg_type = Float64
+        default = 0.1
+
+        "idx"
+        help = "idx of trial"
+        arg_type = Int
+        required = true
+    end
+
+    return parse_args(s)
+end
 
 
 function plot_chain(df, ml_est, latents, col_t, path)
@@ -19,27 +49,18 @@ function plot_chain(df, ml_est, latents, col_t, path)
                                      Gadfly.Geom.vline,
                                      Scale.x_continuous(minvalue = 0, maxvalue = 120)),
                     latents)
-    # last log scores
-    # log_scores = Gadfly.plot(x = 1:120,
-    #                          y = ml_est,
-    #                          xintercept = [col_t],
-    #                          Gadfly.Geom.point,
-    #                          Gadfly.Geom.vline,
-    #                          Scale.x_continuous(minvalue = 0, maxvalue = 120))
-    # log_scores = Gadfly.plot(df, y = :log_score, x = :t, xintercept = [col_t],
-    #                          Gadfly.Geom.histogram2d(ybincount=20),
-    #                          Gadfly.Geom.vline,
-    #                          Scale.x_continuous(minvalue = 0, maxvalue = 120))
-    # plot = vstack(estimates..., log_scores)
     plot = vstack(estimates...)
     compose(compose(context(), rectangle(), fill("white")), plot) |>
         PNG(path);
 end
 
-function process_trial(dataset_path,
-                       trace_path::String,
+function process_trial(particles::Int,
+                       noise::Float64,
                        trial::Int)
 
+    dataset_path = "/databases/exp1.hdf5"
+    dataset_name = first(splitext(basename(dataset_path)))
+    trace_path = "/traces/$(dataset_name)_p_$(particles)_n_$(obs_noise)/$(trial)_c_1.jld2"
     dataset = GalileoRamp.galileo_ramp.Exp1Dataset(dataset_path)
     (scene, state, cols) = get(dataset, trial)
 
@@ -57,9 +78,4 @@ function process_trial(dataset_path,
     viz_path = "$trace_path/$(trial)_viz.gif"
     visualize(scene, gt_pos, preds, nothing, viz_path)
     return nothing;
-end
-
-# process_trial("/databases/exp1.hdf5", "/traces", 1);
-for i = 0:209
-    process_trial("/databases/exp1.hdf5", "/traces/exp1_p_300_n_0.8", i);
 end
