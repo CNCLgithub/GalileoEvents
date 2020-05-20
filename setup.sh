5e61517ad9f184877afb60063b4df5f9359055db
belledon <mbelledonne@gmail.com>
2020-05-20 14:30:02 -0400

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,26 +2,36 @@
 
 . load_config.sh
 
+usage="$(basename "$0") [targets...] -- setup an environmental component of the project according to [default|local].conf
 
-cont=${1:-""}
-conda=${2:-""}
-julia=${3:-""}
+supported targets:
+
+    cont_[pull|build] : either pull the singularity container or build from scratch
+    conda : build the conda environment
+    julia: build julia environment
+"
+
+[ $# -eq 0 ] || [[ "${@}" =~ "help" ]] && echo "$usage"
 
 # container setup
-[ -z "$cont" ] || [ "$cont" = "false" ] && echo "Not touching container"
-[ "$cont" = "pull" ] && wget "https://yale.box.com/shared/static/i5vxp5xghtfb2931fhd4b0ih4ya62o2s.sif" -O "${ENV[cont]}" 
-[ "$cont" = "build" ] || [ "$cont" = "true" ] && echo "building container" && \
+[[ "${@}" =~ "cont_build" ]] || [[ "${@}" =~ "cont_pull" ]] || echo "Not touching container"
+[[ "${@}" =~ "cont_pull" ]] && echo "pulling container" && \
+    wget "https://yale.box.com/shared/static/i5vxp5xghtfb2931fhd4b0ih4ya62o2s.sif" -O "${ENV[cont]}"
+    -O "${ENV[cont]}"
+[[ "${@}" =~ "cont_build" ]] && echo "building container" && \
     SINGULARITY_TMPDIR=/var/tmp sudo -E singularity build "${ENV[cont]}" Singularity
 
 
 # conda setup
-[ -z "$conda" ] || [ "$conda" = "false" ] && echo "Not touching conda"
-[ "$conda" = "build" ] || [ "$conda" = "true" ] && echo "building conda env" && \
+[[ "${@}" =~ "conda" ]] || echo "Not touching conda"
+[[ "${@}" =~ "conda" ]] && echo "building conda env" && \
     singularity exec ${ENV[cont]} bash -c "yes | conda create -p $PWD/${ENV[env]} python=3.6" && \
-    ./run.sh python -m pip install -r requirements.txt
+    ./run.sh python -m pip install -r requirements.txt && \
+    # detectron2 setup (dependent on torch, so out of the requirements.txt
+    # since pip installs in the alphabetical order)
+    ./run.sh python -m pip install -e deps/detectron2
 
 # julia setup
-
-[ -z "$julia" ] || [ "$julia" = "false" ] && echo "Not touching julia"
-[ "$julia" = "build" ] || [ "$julia" = "true" ] && echo "building julia env" && \
+[[ "${@}" =~ "julia" ]] || echo "Not touching julia"
+[[ "${@}" =~ "julia" ]] && echo "building julia env" && \
     ./run.sh julia -e '"using Pkg; Pkg.instantiate()"'
