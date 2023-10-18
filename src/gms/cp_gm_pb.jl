082e2ab2a686d256eef1a5918378feaf6e5140b9
Dominik Glandorf <dominik.glandorf@yale.edu>
2023-10-18 16:11:23 -0400

    experiment with changepoint model

--- src/gms/cp_gm_pb.jl
+++ src/gms/cp_gm_pb.jl
@@ -61,7 +61,7 @@ Current state of the change point model, simulation state and event state
 """
 struct CPState <: GMState
     bullet_state::BulletState
-    active_events::Vector{Int64}
+    active_events::Set{Int64}
 end
 
 ## PRIOR
@@ -94,7 +94,7 @@ end
 """
 initializes belief about all objects and events
 """
-@gen (static) function cp_prior(params::CPParams)
+@gen function cp_prior(params::CPParams)
     # initialize the kinematic state
     latents = params.template.latents
     params_filled = Fill(params, length(latents))
@@ -102,7 +102,7 @@ initializes belief about all objects and events
     bullet_state = setproperties(params.template; latents = new_latents)
 
     # initialize the event state
-    active_events = Vector{EventRelation}()
+    active_events = Set{EventRelation}()
 
     init_state = CPState(bullet_state, active_events)
     return init_state
@@ -113,8 +113,13 @@ Bernoulli weight that event relation holds
 """
 function predicate(t::Type{Collision}, a::RigidBodyState, b::RigidBodyState)
     d = norm(Vector(a.position)-Vector(b.position)) # l2 distance
-    clamp(1.0 - d, 0., 1.)
+    @show d
+    clamp(1.0 - exp(-d), 0., 1.)
 end
+
+# TODO: surface distances, use pybullet (contact maybe not helpful)
+
+# gen functional collections 
 
 """
 update latents of a single element
@@ -130,19 +135,17 @@ update latents of a single element
     return new_latents
 end
 
-
-
 """
 in case of collision: Gaussian drift update of mass and restitution#
 """
-@gen (static) function _collision_clause(pair_idx::Vector{Int64}, latents::Vector{<:BulletElemLatents})
-    new_latents = @trace(update_latents(latents[pair_idx[1]]), :new_latents_a)
-    new_latents = @trace(update_latents(latents[pair_idx[2]]), :new_latents_b)
-    return Type{Collision}
+@gen function _collision_clause(pair_idx::Vector{Int64}, latents::Vector{BulletElemLatents})
+    latents[pair_idx[1]] = @trace(update_latents(latents[pair_idx[1]]), :new_latents_a)
+    latents[pair_idx[2]] = @trace(update_latents(latents[pair_idx[2]]), :new_latents_b)
+    return latents
 end
 
-@gen (static) function _no_event_clause(pair_idx, latents::Vector{BulletElemLatents})
-    return Type{NoEvent}
+@gen function _no_event_clause(pair_idx, latents::Vector{BulletElemLatents})
+    return latents
 end
 
 """
@@ -157,7 +160,7 @@ function clause(::Type{NoEvent})
 end
 
 """
-objects that are already involved in some events should not be involved in new events
+
 """
 function valid_relations(state::CPState, event_concepts::Vector{Type{EventRelation}})
     return event_concepts
@@ -167,45 +170,90 @@ function valid_relations(state::CPState, event_concepts::Vector{Type{EventRelati
     end
 end
 
+## TODO: add Pkg revise
+
+
+# map mcmc kernel (link: https://www.gen.dev/docs/stable/ref/mcmc/#Composing-Stationary-Kernels)
+# set of proposal functions
+
+# change randomly clause choices of mass 
+# revise for which event, modify the categorical
+# e.g. another event type for the same pair or another event for one event type
+
+
+# cognition: computation around events
+# modelling: changepoint model with Switch combinator
+# inference: change types of events to help inference being faster
+
+
 """
 iterate over event concepts and evaluate predicates to active/deactive
 """
 @gen function event_kernel(state::CPState, event_concepts::Vector{Type{<:EventRelation}})
-    # TODO: filter out invalid event relations (e.g., a collision is already active between a and b)
-    #relations = valid_relations(state, event_concepts)
-
-    # activate new events
-    object_pairs = collect(combinations(state.bullet_state.kinematics, 2)) # get pairs of objects
-    # map active events to weights 2D tensor for birth decision
-    
-    weights = [predicate(event_type, a, b) for  event_type in event_concepts for (a, b) in object_pairs]
-    push!(weights, 1 - min(1, sum(weights))) # for no event
-    weights = weights ./ sum(weights)
-    #println(weights)
-
+    println("-------")
+    object_pairs = collect(combinations(state.bullet_state.kinematics, 2))
     pair_idx = repeat(collect(combinations(1:length(state.bullet_state.kinematics), 2)), length(event_concepts))
-    push!(pair_idx, Vector([0,0])) # for no event
+    pair_idx = [[0,0], pair_idx...] # for no event
     #println(pair_idx)
+
+    # map possible events to weight vector for birth decision using the predicates
+    predicates = [predicate(event_type, a, b) for  event_type in event_concepts for (a, b) in object_pairs]
+    print("Predicates: ")
+    println(predicates)
+
+    # randomly draw a starting and an ending event
+    # birth
+    weights = copy(predicates)
+    for idx in state.active_events # active events should not be born again
+        weights[idx-1] = 0
+    end
+    weights = [max(0, 1 - sum(weights)), weights...]
+    # TODO: objects that are already involved in some events should not be involved in other event types as well
+    weights_normalized = weights ./ sum(weights)
+    println(weights_normalized)
     
-    # draw one event overall
-    event_idx = @trace(categorical(weights), :event_idx)
-    active_events = [state.active_events..., event_idx]
-    #push!(active_events, event_idx)
-    #println(active_events)
+    # Draw born event
+    events = vcat(NoEvent, [repeat([event_type], length(object_pairs)) for event_type in event_concepts]...)
 
-    # Switch combinator to evaluate clauses for each event
-    events = vcat([repeat([event_type], length(object_pairs)) for event_type in event_concepts]..., NoEvent)
-    event = @trace(Gen.Switch(map(clause, events)...)(event_idx, pair_idx[event_idx], state.bullet_state.latents), :event)
+    start_event_idx = @trace(categorical(weights_normalized), :start_event_idx)
+    if start_event_idx > 1
+        push!(state.active_events, start_event_idx)
+        print("Event started: ")
+        print(events[start_event_idx])
+        println(pair_idx[start_event_idx])
+    end
 
-    # TODO: some new events kill old events
+    # Evaluate clauses for the born event
+    
+    updated_latents = @trace(Gen.Switch(map(clause, events)...)(start_event_idx, pair_idx[start_event_idx], state.bullet_state.latents), :event)
+    bullet_state = setproperties(state.bullet_state; latents = updated_latents)
 
-    return active_events
+    # death of old active events
+    #println([1.0-predicates[idx] for idx in 1:length(predicates)])
+    # TODO: think about another death probability function, maybe including event age, or add death predicate
+    weights = [(idx+1 in state.active_events && idx+1 != start_event_idx) ? (1.0 - predicates[idx]) : 0.0 for idx in 1:length(predicates)] # dying has the opposite chance of being born
+    #print("Unnormalized death weights: ")
+    #println(weights)
+    weights = [max(0, 1-sum(weights)), weights...] # no event at index 1
+    weights = weights ./ sum(weights) # normalize
+    #print("Normalized death weights: ")
+    #println(weights)
+    end_event_idx = @trace(categorical(weights), :end_event_idx)
+    #println(end_event_idx)
+    if end_event_idx > 1 # nothing happens when no event dies
+        delete!(state.active_events, end_event_idx)
+        print("Ended event: ")
+        print(events[end_event_idx])
+        println(pair_idx[end_event_idx])
+    end 
+
+    return state.active_events, bullet_state
 end
 
 """
 for one object, observe the noisy position in every dimension
 """
-@gen (static) function observe_position(k::RigidBodyState, noise::Float64)
+@gen function observe_position(k::RigidBodyState, noise::Float64)
     pos = k.position
     # add noise to position
     obs = @trace(broadcasted_normal(pos, noise), :positions)
@@ -215,12 +263,12 @@ end
 """
 for one time step, run event and physics kernel
 """
-@gen (static) function kernel(t::Int, prev_state::CPState, params::CPParams)
+@gen function kernel(t::Int, prev_state::CPState, params::CPParams)
     # event kernel
-    active_events = @trace(event_kernel(prev_state, params.event_concepts), :events)
+    active_events, bullet_state = @trace(event_kernel(prev_state, params.event_concepts), :events)
 
     # simulate physics for the next step based on the current state and observe positions
-    bullet_state::BulletState = PhySMC.step(params.sim, prev_state.bullet_state)
+    bullet_state::BulletState = PhySMC.step(params.sim, bullet_state)
     obs = @trace(Gen.Map(observe_position)(bullet_state.kinematics, Fill(params.obs_noise, params.n_objects)), :observe)
 
     next_state = CPState(bullet_state, active_events)
@@ -230,7 +278,7 @@ end
 """
 generate physical scene with changepoints in the belief state
 """
-@gen (static) function cp_model(t::Int, params::CPParams)
+@gen function cp_model(t::Int, params::CPParams)
     # initalize the kinematic and event state
     init_state = @trace(cp_prior(params), :prior)
    
--- test/gms/cp_gm.jl
+++ test/gms/cp_gm.jl
@@ -1,9 +1,10 @@
+using Revise
 using Gen
 using GalileoEvents
 
 mass_ratio = 2.0
 obj_frictions = (0.3, 0.3)
-obj_positions = (0.5, 1.5)
+obj_positions = (0.5, 1.2)
 
 mprior = MaterialPrior([unknown_material])
 pprior = PhysPrior((3.0, 10.0), # mass
@@ -16,16 +17,35 @@ t = 120
 function forward_test()
     client, a, b = ramp(mass_ratio, obj_frictions, obj_positions)
     event_concepts = Type{<:EventRelation}[Collision]
-    cp_params = CPParams(client, [a,b, b], mprior, pprior, event_concepts, obs_noise)
-    trace, _ = Gen.generate(cp_model, (t, cp_params))
+    cp_params = CPParams(client, [a,b], mprior, pprior, event_concepts, obs_noise)
+    addr = :prior => :objects => 1 => :mass
+    cm = Gen.choicemap(addr => 30)
+    trace, _ = Gen.generate(cp_model, (t, cp_params), cm)
+    #display(get_choices(trace))
+end
+
+# constrained generation
+function constrained_test()
+    client, a, b = ramp(mass_ratio, obj_frictions, obj_positions)
+    event_concepts = Type{<:EventRelation}[Collision]
+    cp_params = CPParams(client, [a,b], mprior, pprior, event_concepts, obs_noise)
+
+    addr = 10 => :events => :start_event_idx
+    cm = Gen.choicemap(addr => 1)
+    trace, _ = Gen.generate(cp_model, (t, cp_params), cm)
     display(get_choices(trace))
 end
 
+# gen regenerate
+
 
 function update_test()
+    t = 120
+
     client, a, b = ramp(mass_ratio, obj_frictions, obj_positions)
-    mc_params = MCParams(client, [a,b], mprior, pprior, obs_noise)
-    trace, _ = Gen.generate(mc_gm, (t, mc_params))
+    event_concepts = Type{<:EventRelation}[Collision]
+    cp_params = CPParams(client, [a,b], mprior, pprior, event_concepts, obs_noise)
+    trace, _ = Gen.generate(cp_model, (t, cp_params))
 
     addr = :prior => :objects => 1 => :mass
     cm = Gen.choicemap(addr => trace[addr] + 3)
@@ -41,5 +61,68 @@ function update_test()
 end
 
 
-forward_test()
+function update_test_2()
+    t = 120
+
+    client, a, b = ramp(mass_ratio, obj_frictions, obj_positions)
+    event_concepts = Type{<:EventRelation}[Collision]
+    cp_params = CPParams(client, [a,b], mprior, pprior, event_concepts, obs_noise)
+    trace, _ = Gen.generate(cp_model, (t, cp_params))
+
+    addr = :prior => :objects => 1 => :mass
+    cm = Gen.choicemap(addr => trace[addr] + 3)
+    trace2, _ = Gen.update(trace, cm)
+
+    # compare final positions
+    t=120
+    pos1 = Vector(get_retval(trace)[t].bullet_state.kinematics[1].position)
+    pos2 = Vector(get_retval(trace2)[t].bullet_state.kinematics[1].position)
+    @assert pos1 != pos2
+
+    # TODO: more tests that include events
+
+    return trace, trace2
+end
+
+
+# test switch combinator in terms of gen's reaction to proposed changes
+
+# toy model for dealing with complexing
+# random walk with 2 delta functions (gaussian vs uniform) chosen by switch
+# initial trace is changed by a mh proposal for switch index
+# static first, unfold complexity second step
+
+@gen function function1() 
+    v ~ normal(0., 1.)
+end
+
+@gen function function2()
+    v ~ uniform(-1., 1.)
+end
+
+@gen function choose_delta(i::Int64)
+    function_idx = @trace(categorical([0.5, 0.5]), (:function, i))
+    delta ~ Gen.Switch(function1, function2)(function_idx)
+end
+
+@gen function switch_model()
+    n = @trace(poisson(5), :n)
+    total = 0.
+    for i=1:n
+        function_idx = @trace(categorical([0.5, 0.5]), (:function, i))
+        total += @trace(Gen.Switch(function1, function2)(function_idx), (:x, i))
+    end
+    @trace(normal(total, 1.), :y)
+    total
+end
+
+function switch_test()
+    trace, _ = Gen.generate(switch_model, ())
+    display(get_choices(trace))
+end
+
+switch_test()
+
+#forward_test()
+#constrained_test()
 #update_test()
